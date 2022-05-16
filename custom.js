1b24af86f8e0a2f6265ac6c53835772ea467e261
Adriano Orlando <adrianoorlando04@gmail.com>
2022-05-16 15:55:33 +1200

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,10 @@
+function play(sound){
+    var audio = document.getElementById("audio");
+    audio.setAttribute('src', sound);
+    audio.play();
+}
+
+function stopAudio(){
+   var audio = document.getElementById("audio");
+   audio.pause();
+}
