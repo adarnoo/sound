9f08b8f82b5eaa71d38d54ade093120cca2570ea
Adriano Orlando <adrianoorlando04@gmail.com>
2022-06-12 18:37:41 +1200

    Add files via upload

--- custom.js
+++ custom.js
@@ -1,3 +1,5 @@
+// Audio Buttons
+
 function play(sound){
     var audio = document.getElementById("audio");
     audio.setAttribute('src', sound);
@@ -7,4 +9,11 @@ function play(sound){
 function stopAudio(){
    var audio = document.getElementById("audio");
    audio.pause();
+}
+
+// Toggle Landing Image
+
+function myFunction() {
+  var element = document.getElementById("tw-toggle");
+  element.classList.toggle("tw-toggle--close");
 }
